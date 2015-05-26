9940251c7b9d0f22ab97ba433d2906568953a0a1
Jean-Michel Decombe <jm@decombe.com>
2015-05-25 20:50:41 -0700

    Complete more functional exercises

--- functional.js
+++ functional.js
@@ -3,7 +3,7 @@ function upperCaser(input) {
 	return input.toUpperCase()
 }
 // HIGHER ORDER FUNCTIONS
-function repeat(operation, num) {
+function proceduralRepeat(operation, num) {
 	while(num-- > 0) {
 		operation()
 	}
@@ -11,7 +11,7 @@ function repeat(operation, num) {
 function recursiveRepeat(operation, num) {
 	if (num > 0) {
 		operation()
-		repeat(operation, num - 1)
+		recursiveRepeat(operation, num - 1)
 	}
 }
 // MAP
@@ -21,7 +21,7 @@ function doubleAll(numbers) {
 	})
 }
 // FILTER
-function getShortMessages (array) {
+function getShortMessages(array) {
 	return array.filter(function (item) {
 		return item.message.length < 50
 	}).map(function (item) {
@@ -62,11 +62,99 @@ function duckCount() {
 }
 // PARTIAL APPLICATION WITHOUT BIND
 var slice = Array.prototype.slice
-function logger(namespace) {
+function applyLogger(namespace) {
 	return function () {
 		console.log.apply(console, [namespace].concat(slice.call(arguments)))
-    }
+	}
 }
 // PARTIAL APPLICATION WITH BIND
+function bindLogger(namespace) {
+	return console.log.bind(console, namespace)
+}
+// MAP WITH REDUCE
+function mapWithReduce(input, operation) {
+	return input.reduce(function (previous, current, index, array) {
+		return previous.concat(operation.call(null, current, index, array))
+	}, [])
+}
+// FUNCTION SPY
+function spy(target, method) {
+	var result = {count:0}
+	var fn = target[method]
+	target[method] = function () {
+		result.count++
+		return fn.apply(this, arguments)
+	}
+	return result;
+}
+// BLOCKING EVENT LOOP
+function repeatWithoutBlocking(operation, num) {
+	if (num <= 0) {
+		return
+	}
+	operation()
+	if (num % 10 === 0) {
+		setTimeout(function () {
+			repeatWithoutBlocking(operation, --num)
+		})
+	} else {
+		repeatWithoutBlocking(operation, --num)
+	}
+}
+// TRAMPOLINE
+function repeat(operation, num) {
+	if (num <= 0) {
+		return
+	}
+	return function () {
+		operation()
+		return repeat(operation, --num)
+	}
+}
+function trampoline(fn) {
+	while (fn && typeof fn === 'function') {
+		fn = fn()
+	}
+}
+function exportedTrampoline(operation, num) {
+	trampoline(function () {
+		return repeat(operation, num)
+	})
+}
+// ASYNC LOOPS
+function loadUsers(userIds, load, done) {
+//	var users = []
+//	for (var i = 0; i < userIds.length; i++) {
+//		users.push(load(userIds[i]))
+//	}
+//	return users
+	var users = []
+	var count = 0
+	userIds.forEach(function (id, index) {
+		load(id, function (user) {
+			users[index] = user
+			if(++count === userIds.length) {
+				return done(users)
+			}
+		})
+	})
+}
+//RECURSION
+function getDependencies(tree, result) {
+	result = result || []
+	var dependencies = tree && tree.dependencies || []
+	Object.keys(dependencies).forEach(function (name) {
+		var descriptor = name + "@" + dependencies[name].version
+		if (result.indexOf(descriptor) === -1) {
+			result.push(descriptor)
+			getDependencies(dependencies[name], result)
+		}
+	})
+	return result.sort()
+}
+// CURRYING
 
-module.exports = logger
+// FUNCTION CALL
+
+// EXPORTS
+module.exports = getDependencies
